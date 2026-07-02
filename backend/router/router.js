fff1b3998e85c64851988bfef15d6ad2c12d8f86
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-07-02 03:25:51 +0300

    fix(security): protect /dataUser route with verifyToken middleware

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,26 +1,15 @@
 const express = require("express");
 const AllController = require("../collection/sign");
 const fandoavamBola = require("../collection/paye");
-const student = require("../model/client");
+const verifyToken = require("../middleware/verifyToken");
 const router = express.Router();
 
-
-router.get("/dataUser", AllController.dataUser);
+// Route proteger : seul un utilisateur avec un token valide peut acceder aux donnees users
+router.get("/dataUser", verifyToken, AllController.dataUser);
 router.post("/signup", AllController.signup);
 router.post("/login", AllController.login);
-/* router.get("/aboutClient", AllController.isAuthentification, async (req, res, next) => {
-    try {
-        const user = await student.findById(req.user._id);
-        res.json(user);
-    } catch (error) {
-        res.status(500).json({ message: "Erreur lors de la récupération des données utilisateur" });
-    }
-}) */
 router.post("/payement", fandoavamBola.payement);
 router.post("/payementMvola", fandoavamBola.payementMvola);
 router.post("/ChangePass/:id", AllController.ChangePass);
-/* router.get("/getToken",AllController.getToken); */
-
-
 
 module.exports = router;
