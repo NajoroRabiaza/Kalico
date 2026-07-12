60a812c9de023101f2f76e30d17649e22dd7eeee
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-07-12 22:05:52 +0300

    chore: remove dead imports, unused state and fix Windows path in Card and Cartes components

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,52 +1,38 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { useState } from "react";
 import "./cards.css";
-import Panier from "../page/Panier";
-import produitContext from "../page/home";
 
 const Cards = ({ item, handleClick }) => {
-  const { id, nom, description, prix, img, quantite } = item;
-  const [article, setarticle] = useState([]);
+  const { nom, description, prix, quantite } = item;
   const [quantity, setQuantity] = useState(1);
 
-  const increment = () => setQuantity(quantity + 1);
-  const decrement = () => {
-    quantity > 1 && setQuantity(quantity - 1);
-  };
+  const increment = () => setQuantity((prev) => prev + 1);
+  const decrement = () => setQuantity((prev) => (prev > 1 ? prev - 1 : 1));
 
   return (
     <div className="cards_container">
-      {/* <img src="src\image\font.webp" alt="font" className="font" /> */}
-      <div className="card" onMouseLeave={() => setShowStars(false)}>
+      <div className="card">
         <div className="tete">
-          <img className="carteimage" src={img} alt="Image" />
+          <img className="carteimage" src={item.img} alt={nom} />
         </div>
-
         <div className="body">
           <div className="card_text">
             <p className="titre_card">{nom}</p>
-
             <p className="prix">
-              <b>{prix} Ar</b>{" "}
+              <b>{prix} Ar</b>
             </p>
             <div className="quantity-controls">
-              <button className="quantity-controls1 " onClick={decrement}>
-                -
-              </button>
+              <button className="quantity-controls1" onClick={decrement}>-</button>
               <span className="qtt">{quantity}</span>
-              <button className="quantity-controls2 " onClick={increment}>
-                +
-              </button>
+              <button className="quantity-controls2" onClick={increment}>+</button>
             </div>
             <p className="stock_restant">
               <b>Dispo :</b> {quantite}
             </p>
-
             <p className="description">
-              <b>Ingredient: </b>
-              {description}
+              <b>Ingredient: </b>{description}
             </p>
             <button
-              onClick={() => handleClick({ ...item, quantity: quantity || 1 })}
+              onClick={() => handleClick({ ...item, quantity })}
               className="ajouter"
             >
               Ajouter
--- src/component/cartes.jsx
+++ src/component/cartes.jsx
@@ -1,53 +1,39 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { useState } from 'react';
 import './cartes.css';
-import Panier from '../page/Panier';
-import produitContext from '../page/home';
-import "./cartes.css"
-import { Row, Col } from 'react-bootstrap';
-
-
-{/*export const varyContext = createContext();*/ }
-
 
 const Cartes = ({ item, handleClick }) => {
-  const { id, nom, description, prix, img } = item;
-  const [article, setarticle] = useState([]);
+  const { nom, description, prix, img } = item;
   const [quantity, setQuantity] = useState(1);
-  
 
-  const increment = () => setQuantity(quantity + 1);
-  const decrement = () => {
-    (quantity > 1) && setQuantity(quantity - 1)
-  };
+  const increment = () => setQuantity((prev) => prev + 1);
+  const decrement = () => setQuantity((prev) => (prev > 1 ? prev - 1 : 1));
 
   return (
     <div className='cartes_container'>
-      <img src="src\image\font.webp" alt="font" className="fonts" />
-      <div className='menus'></div>
       <div className='cartes'>
-
-        <img className="cartesimage" src={img} alt="Image" />
+        <img className="cartesimage" src={img} alt={nom} />
         <div className='cartes_text'>
-          <p className='titles_card' >{nom}</p>
+          <p className='titles_card'>{nom}</p>
           <p className='descriptions'><b>Ingredient:</b> {description}</p>
-          <p className='price'> Prix : <b style={{ color: 'green' }}>{prix}  Ar</b></p>
+          <p className='price'>Prix : <b style={{ color: 'green' }}>{prix} Ar</b></p>
           <p className="stock_rest">
-              <b>Dispo :    </b>{ item.quantite} 
-            </p>
+            <b>Dispo :</b> {item.quantite}
+          </p>
           <div className="quantite-controls">
-            <button className='quantite-controls1 ' onClick={decrement}>-</button>
+            <button className='quantite-controls1' onClick={decrement}>-</button>
             <span>{quantity}</span>
-            <button className='quantite-controls2 ' onClick={increment}>+</button>
-
+            <button className='quantite-controls2' onClick={increment}>+</button>
           </div>
-          {/*<varyContext.Provider value={article} className="bouttonA"> */}
-          <button onClick={() => handleClick({ ...item, quantity })} className='boutton'>Ajouter au panier</button>
-          {/*</varyContext.Provider>*/}
+          <button
+            onClick={() => handleClick({ ...item, quantity })}
+            className='boutton'
+          >
+            Ajouter au panier
+          </button>
         </div>
       </div>
     </div>
-
-  )
-}
+  );
+};
 
 export default Cartes;
