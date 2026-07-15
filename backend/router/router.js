743b0f16e352e2e2c0e9de3c012cc1dd53430ff6
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-07-15 22:56:19 +0300

    chore: remove duplicate payment system, clean up router

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const AllController = require('../collection/sign');
-const fandoavamBola = require("../collection/paye");
 const verifyToken = require("../middleware/verifyToken");
 const router = express.Router();
 
-// Route proteger : seul un utilisateur avec un token valide peut acceder aux donnees users
+// Route proteger : seul un user avec un token valide peut acceder aux donnees users
 router.get("/dataUser", verifyToken, AllController.dataUser);
 router.post("/signup", AllController.signup);
 router.post("/forgotPassword", AllController.forgotPassword);
 router.post("/login", AllController.login);
-router.post("/payement", fandoavamBola.payement);
-router.post("/payementMvola", fandoavamBola.payementMvola);
 router.post("/ChangePass/:id", AllController.ChangePass);
 
 module.exports = router;
