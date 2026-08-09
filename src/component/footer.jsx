3a01cc835796fa2f920c90c5306f77b986e20caf
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-08-09 19:05:37 +0300

    fix(footer): simplify layout, remove floating card, use clean single dark container

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -10,35 +10,35 @@ function Foot() {
   return (
     <footer className="StyleFooter">
       <div className="footer_3D">
-        <img src={logo} alt="logo" className="logo_ftr" />
-        <h3 className="contact">Contactez Nous:</h3>
-        <h3>
+        <img src={logo} alt="logo Kalico" className="logo_ftr" />
+
+        {/* Bloc contact regroupe titre et icones proprement */}
+        <div className="footer-contact-block">
+          <h3 className="contact">Contactez Nous</h3>
           <div className="item_ftr">
-            <a href="https://www.facebook.com/HaiRunUniversity">
-              <img src={icon_Facebook} className="icon_ftr" />
+            <a href="https://www.facebook.com/HaiRunUniversity" target="_blank" rel="noreferrer">
+              <img src={icon_Facebook} className="icon_ftr" alt="Facebook" />
             </a>
-            <a href="contact@grande-ecole-it.com">
-              <img src={icon_mail} className="icon_ftr" />
+            <a href="mailto:contact@grande-ecole-it.com">
+              <img src={icon_mail} className="icon_ftr" alt="Email" />
             </a>
-            <a href="https://maps.app.goo.gl/gaSQXBQ5bQrMGUCS8">
-              <img src={icon_lieu} className="icon_ftr" />
+            <a href="https://maps.app.goo.gl/gaSQXBQ5bQrMGUCS8" target="_blank" rel="noreferrer">
+              <img src={icon_lieu} className="icon_ftr" alt="Localisation" />
             </a>
             <a href="#">
-              <img src={icon_contact} className="icon_ftr" />
+              <img src={icon_contact} className="icon_ftr" alt="Contact" />
             </a>
           </div>
-        </h3>
+        </div>
       </div>
+
       <div className="txt_ftr">
         <h1>
-          Developée par :
-          <>
-            {" "}
-                Bug<span>409</span>
-          </>
+          Developée par : Bug<span>409</span>
         </h1>
       </div>
     </footer>
   );
 }
+
 export default Foot;
