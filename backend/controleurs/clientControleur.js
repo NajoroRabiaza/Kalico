243257d185b8e43e5dfda2e899423bed8e922a21
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-08-14 02:40:42 +0300

    fix(security): exclure le champ password de la reponse getClients

--- backend/controleurs/clientControleur.js
+++ backend/controleurs/clientControleur.js
@@ -2,7 +2,7 @@ const Client = require("../model/client");
 
 const getClients = async (req, res) => {
   try {
-    const clients = await Client.find();
+    const clients = await Client.find().select("-password");
     res.json(clients);
   } catch (error) {
     res.status(500).json({ message: error.message });
