6c3e16c66851551d42e70404d0d41fc205fc87e5
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-08-27 22:29:06 +0300

    feat(topnav): afficher le titre de la page courante dans le header

--- src/admin/components/TopNav/TopNav.jsx
+++ src/admin/components/TopNav/TopNav.jsx
@@ -1,10 +1,22 @@
+import { useLocation } from "react-router-dom";
 import profileImg from "../../assets/images/profile-02.png";
 import "./top-nav.css";
 
+const pageTitles = {
+  "/admin/dashboard": "Tableau de bord",
+  "/admin/Commande": "Les Commandes",
+  "/admin/Products": "Les Produits",
+  "/admin/Clients": "Nos Clients",
+};
+
 const TopNav = () => {
+  const { pathname } = useLocation();
+  const titre = pageTitles[pathname] ?? "Administration";
+
   return (
     <div className="top__nav">
       <div className="top__nav-wrapper">
+        <h2 className="top__nav-titre">{titre}</h2>
         <div className="top__nav-right">
           <div className="profile">
             <img src={profileImg} alt="Profil administrateur" />
